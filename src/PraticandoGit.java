3808070f1a00578c56ca7067f5a759b892fc7899
Fillipe Santtos <fillipesanttos@proton.me>
2025-04-20 11:46:28 -0300

    Dia 1 Praticando Git

---
+++ src/PraticandoGit.java
@@ -0,0 +1,10 @@
+public class PraticandoGit {
+
+    public static void main (String[] args){
+        System.out.println("Hello World");
+    }
+}
+
+//Dia 1 praticando Git
+    //Criar repositorio, atualizar informacoes, criar tag, branch, merge, fazer push
+
