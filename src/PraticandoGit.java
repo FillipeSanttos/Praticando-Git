101c40f9d4d0deaf481787150df792415ff9f649
Fillipe Santtos <fillipesanttos@proton.me>
2025-04-21 14:10:51 -0300

    Dia 02 praticando git (amend)

--- src/PraticandoGit.java
+++ src/PraticandoGit.java
@@ -1,10 +1,11 @@
 public class PraticandoGit {
 
-    public static void main (String[] args){
-        System.out.println("Hello World");
-    }
+public static void main (String[] args){
+    System.out.println("Hello World!");
+}
 }
 
 //Dia 1 praticando Git
     //Criar repositorio, atualizar informacoes, criar tag, branch, merge, fazer push
     //Modificação para criar o merge
+//Dia 2 praticando git
