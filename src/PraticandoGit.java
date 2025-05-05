775a0baf1507c0e83e71de51032756ce35795768
Fillipe Santtos <fillipesanttos@proton.me>
2025-05-05 19:12:49 -0300

    Alinhar código

--- src/PraticandoGit.java
+++ src/PraticandoGit.java
@@ -1,29 +1,26 @@
 public class PraticandoGit {
-
-
-public static void main (String[] args){
-System.out.println("Hello World!!!!");
-}
-
+    public static void main(String[] args) {
+        System.out.println("Hello World!!!!");
+    }
 }
 
 
 //Dia 1 praticando Git
-    //Criar repositorio, atualizar informacoes, criar tag, branch, merge, fazer push
-    //Modificação para criar o merge
+//Criar repositorio, atualizar informacoes, criar tag, branch, merge, fazer push
+//Modificação para criar o merge
 //Dia 2 praticando git
-    //criacao de branch dia02, realizar merge
+//criacao de branch dia02, realizar merge
 //Dia 03 praticando git
-    //Modificações para add e commit
-    //Modificacao para branch, fazer merge
+//Modificações para add e commit
+//Modificacao para branch, fazer merge
 //Dia 04 praticando git
-    //Modificacoes para branch, fazer merge
+//Modificacoes para branch, fazer merge
 //Dia 05 praticando git
-    //atualizacao de informacoes
-    //novo branch, fazer merge
+//atualizacao de informacoes
+//novo branch, fazer merge
 //Dia 06 praticando git
-    //atualização de informações
-    //novo branch, fazer merge
+//atualização de informações
+//novo branch, fazer merge
 //Dia 07 praticando git
-    //atualizacao de informacoes
-    //novo branch, fazer merge, deletar branch dia anterior
+//atualizacao de informacoes
+//novo branch, fazer merge, deletar branch dia anterior
