cbbdeb3ad819ee8a524dd945ce0db255c57772ff
Fillipe Santtos <fillipesanttos@proton.me>
2025-05-06 16:06:01 -0300

    Dia 08 Praticando Git e Conceitos

---
+++ src/estudos/praticadegit/novosconceitos/praticandoconceitos.java
@@ -0,0 +1,20 @@
+package estudos.praticadegit.novosconceitos;
+
+// COMENTARIO LINHA UNICA Dia 08 Praticando Git e Novos conceitos
+
+/* COMENTARIO VARIAS LINHAS
+    Criando classe,
+    comentarios
+    e utilizando git
+ */
+
+/**
+ * COMENTARIO EM <b> Javadoc </b>
+ */
+
+
+public class praticandoconceitos {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
--- src/estudos/praticadegit/testesdiarios/PraticandoGit.java
+++ src/estudos/praticadegit/testesdiarios/PraticandoGit.java
@@ -1,3 +1,5 @@
+package estudos.praticadegit.testesdiarios;
+
 public class PraticandoGit {
     public static void main(String[] args) {
         System.out.println("Hello World!!!!");
